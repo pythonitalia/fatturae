69d251e4267f1686913f8f8304259ab43a11106e
Marco Aceti <personale@marcoaceti.it>
2019-01-03 14:27:35 +0100

    Add invoice and address models to the admin panel

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Sender
+from .models import Sender, Address, Invoice
 
 
 admin.site.register(Sender)
+admin.site.register(Address)
+admin.site.register(Invoice)
